0b1500c42c27f1561bab28cdd30c12df5ae04177
Yuliya Banko <yuliyabanko@gmail.com>
2024-12-18 10:17:49 +0300

    Junit reporter added

--- cypress/e2e/2/login_sqlveridier_5.3.4.cy.js
+++ cypress/e2e/2/login_sqlveridier_5.3.4.cy.js
@@ -1,4 +1,5 @@
 const testData = require("../../fixtures/logindata.json");
+const regData = require("../../fixtures/registrdata.json");
 
 describe("LoginSqlverifier", () => {
     it("successfulLogin", () => {
@@ -13,3 +14,23 @@ describe("LoginSqlverifier", () => {
         });             
     });
 });
+
+describe("Registration", () => {
+    it("successfulRegistration", () => {
+        cy.registrSQL(
+            Cypress.env('username1'),
+            Cypress.env('newpassword1'),
+            Cypress.env('newpassword2'),
+            Cypress.env('email')
+        );
+        cy.get(".invalid-feedback").should('not.exist');              
+    });
+
+    it("unsuccessfulRegistration", () => {
+        regData.forEach((item) => {
+            cy.registrSQL(item.username1, item.newpassword1, item.newpassword2, item.email);
+            cy.get(".invalid-feedback").should('exist');  
+        });             
+    });
+});
+
